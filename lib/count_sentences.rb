e70f3941b1a2a27df632218db3c0c121f95a4dcf
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-12 21:14:30 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,11 +3,13 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
+end
   end
 
   def question?
-
+    self.end_with?("?")
+    end
   end
 
   def exclamation?
@@ -17,4 +19,23 @@ class String
   def count_sentences
 
   end
+end
+
+
+class Dog
+ 
+  attr_accessor :name, :owner
+ 
+  def initialize(name)
+    @name = name
+  end
+ 
+  def bark
+    "Woof!"
+  end
+ 
+  def get_adopted(owner_name)
+    self.owner = owner_name
+  end
+ 
 end
