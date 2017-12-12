5d31494236a5c52ce5c7d6bbfdd64789dd5a9de7
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-12 21:14:52 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -22,20 +22,4 @@ end
 end
 
 
-class Dog
- 
-  attr_accessor :name, :owner
- 
-  def initialize(name)
-    @name = name
-  end
- 
-  def bark
-    "Woof!"
-  end
- 
-  def get_adopted(owner_name)
-    self.owner = owner_name
-  end
- 
-end
+
